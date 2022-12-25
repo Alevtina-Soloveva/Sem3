64956d583d77feb5a5be7456c0fd94b4b8437a83
unknown <happyjaskass@mail.ru>
2022-12-25 15:48:57 +0500

    Ex23 done

---
+++ Ex23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Write your number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i < number)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+    i++;
+}
